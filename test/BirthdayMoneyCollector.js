19831b642cefbebfcb5175c8fbec198b5317b70c
onurcan.avci <onurcan.avci@trendyol.com>
2022-06-09 00:17:15 +0300

    CT-1 Add default contract get functions test

--- test/BirthdayMoneyCollector.js
+++ test/BirthdayMoneyCollector.js
@@ -1,8 +1,46 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// Tests will be added
-describe("BirthdayMoneyCollector contract", function () {
-  it("should test anything", async function () {
-    expect(1).to.equal(1);
+describe("Birthday Money Collector contract", function () {
+  let contractFactory;
+  let birthdayContract;
+
+  let mockBitrhdayChildName;
+  let mockGiftAmount;
+
+  beforeEach(async function () {
+    mockBitrhdayChildName = "Onur";
+    mockGiftAmount = ethers.utils.parseEther("100000");
+    // Get the ContractFactory and Signers here.
+    contractFactory = await ethers.getContractFactory("BirthdayGiftCollector");
+
+    // To deploy our contract, we just have to call Contract.deploy() and await
+    // for it to be deployed(), which happens once its transaction has been
+    // mined.
+    birthdayContract = await contractFactory.deploy(
+      mockBitrhdayChildName,
+      mockGiftAmount
+    );
+  });
+
+  it("Should return the right birthday child name", async function () {
+    expect(await birthdayContract.getBirthdayChildName()).to.equal(
+      mockBitrhdayChildName
+    );
+  });
+
+  it("Should return the right birthday money amount", async function () {
+    expect(await birthdayContract.getGiftAmount()).to.equal(mockGiftAmount);
+  });
+
+  it("Should default return 0 in getContractBalance", async function () {
+    mockDefaultContractBalance = ethers.utils.parseEther("0");
+    expect(await birthdayContract.getContractBalance()).to.equal(
+      mockDefaultContractBalance
+    );
+  });
+
+  it("Should default return empty list in getParticipantList", async function () {
+    expect(await birthdayContract.getParticipantList()).to.length(0);
   });
 });
