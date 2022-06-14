9fec7c10a859fbb5315c192056b91523eb16c83a
Fatih Altuntaş <altuntasfatih42@gmail.com>
2022-06-14 21:56:31 +0300

    Add deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
+require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
 
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
